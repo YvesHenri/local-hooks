f792c14fa843a29c85be060f3d6496daed3d278d
Yves Calaci <yves.henri@netpos.com.br>
2021-03-07 00:52:11 -0300

    bleh

--- DLL/main.cpp
+++ DLL/main.cpp
@@ -1,111 +1,131 @@
 #include <Windows.h>
-#include <string>
 #include <iostream>
 
-#include "client.hpp"
-
 #pragma data_seg(".dll")
-// 
+
 #pragma comment(linker, "/SECTION:.dll,RWS")
 #pragma data_seg()
 
-HHOOK _hookHandle = NULL;
-HINSTANCE _moduleHandle = NULL;
-HookClient _hookClient;
+HANDLE pipe = NULL;
+HHOOK hook = NULL;
+HINSTANCE dll = NULL;
 
 void Log(const char* message)
 {
     FILE* file = NULL;
 
-    freopen_s(&file, "x.txt", "a", stdout);
+    freopen_s(&file, "logs.txt", "a", stdout);
 
     if (file)
     {
         std::cout << message << std::endl;
-
         fclose(file);
     }
+
+}
+struct Request
+{
+    int code;
+    WPARAM wParam;
+    LPARAM lParam;
+};
+
+BOOL APIENTRY DllMain(HMODULE module, DWORD reason, LPVOID reserved)
+{
+    if (reason == DLL_PROCESS_ATTACH)
+    {
+        dll = module;
+    }
+
+    return TRUE;
 }
 
 LRESULT CALLBACK HookProc(int code, WPARAM wParam, LPARAM lParam)
 {
-    if (code > 0)
+    if (code >= 0)
     {
-        if (wParam == WM_RBUTTONUP)
+        if (pipe != NULL && pipe != INVALID_HANDLE_VALUE)
         {
-            Log("Right up");
-        }
-        if (wParam == WM_MBUTTONUP)
-        {
-            Log("Middle up");
-        }
-        if (wParam == WM_LBUTTONUP)
-        {
-            Log("Left up");
-        }
+            auto requestSize = 0ul;
+            auto request = Request { code, wParam, lParam };
 
-        _hookClient.log("Logging message from HookProc");
+            // Send hook data
+            if (WriteFile(pipe, &request, sizeof(request), &requestSize, NULL))
+            {
+                auto responseSize = 0ul;
+                auto response = 0l;
 
-        if (_hookClient.connected())
-        {
-            Log("Sending hook data...");
-            _hookClient.send(code, wParam, lParam);
+                // Await server reply
+                if (ReadFile(pipe, &response, sizeof(response), &responseSize, NULL))
+                {
+                    return response;
+                }
+                else
+                {
+                    char buffer[32];
+                    _itoa_s(GetLastError(), buffer, 10);
+                    Log("Read");
+                    Log(buffer);
+                }
+            }
+            else
+            {
+                char buffer[32];
+                _itoa_s(GetLastError(), buffer, 10);
+                Log("Write");
+                Log(buffer);
+            }
         }
-        else
-            Log("Can not send hook data. Client is disconnected.");
     }
 
-    return CallNextHookEx(_hookHandle, code, wParam, lParam);
+    // Fallback if server can not dispatch the request
+    return CallNextHookEx(hook, code, wParam, lParam);
 }
 
-extern "C" __declspec(dllexport) 
-HHOOK Install(int idHook, HWND window, const char* pipeServerName)
+extern "C" __declspec(dllexport)
+HHOOK Install(int idHook, HWND window, const char* pipeName)
 {
-    if (_hookHandle == NULL && !_hookClient.connected())
+    if (hook == NULL && pipe == NULL)
     {
-        auto targetProcessId = 0ul;
-        auto targetProcessThreadId = GetWindowThreadProcessId(window, &targetProcessId);
+        SetLastError(0ul);
 
-        if (_hookClient.connect(pipeServerName))
+        pipe = CreateFileA(pipeName, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+
+        // If no pipe is available, wait 3 seconds to retry
+        if (GetLastError() == ERROR_PIPE_BUSY)
         {
-            _hookHandle = SetWindowsHookExA(idHook, HookProc, _moduleHandle, targetProcessThreadId);
+            if (!WaitNamedPipeA(pipeName, 3000))
+            {
+                pipe = NULL;
+                return NULL;
+            }
+        }
+
+        // Install hook if connected
+        if (pipe != NULL && pipe != INVALID_HANDLE_VALUE)
+        {
+            auto targetProcessId = 0ul;
+            auto targetProcessThreadId = GetWindowThreadProcessId(window, &targetProcessId);
+
+            hook = SetWindowsHookExA(idHook, HookProc, dll, targetProcessThreadId);
         }
     }
 
-    return _hookHandle;
+    return hook;
 }
 
 extern "C" __declspec(dllexport)
 bool Uninstall()
 {
-    if (_hookClient.connected())
+    if (hook != NULL && UnhookWindowsHookEx(hook))
     {
-        if (_hookClient.disconnect())
+        hook = NULL;
+
+        if (pipe != NULL && CloseHandle(pipe))
         {
-            if (_hookHandle)
-            {
-                UnhookWindowsHookEx(_hookHandle);
-                _hookHandle = NULL;
-            }
+            pipe = NULL;
         }
-    }    
-
-    return _hookHandle == NULL;
-}
-
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
-{
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
-        _moduleHandle = hModule;
-        break;
-    case DLL_PROCESS_DETACH:
-        break;
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-        break;
     }
 
-    return TRUE;
+    return pipe == NULL && hook == NULL;
 }
